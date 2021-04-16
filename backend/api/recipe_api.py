eebdde520a384afeb3d54d7797c5b961cb2f91c2
chriskok <pherkok@gmail.com>
2021-04-16 16:51:37 -0500

    Updated API to pull seasonal/local foods

--- backend/api/recipe_api.py
+++ backend/api/recipe_api.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 import pandas as pd
 import numpy as np
 import json
+from ast import literal_eval
 
 app = FastAPI()
 
 df = pd.read_csv('../data/recipes/RAW_recipes.csv')
 df = df.dropna()
+season_df = pd.read_csv('../data/seasonality.csv')
 
 def query_df(query, k):
     search_list = query.split(" ")
@@ -23,7 +25,14 @@ def query_df(query, k):
 
 
 @app.get("/recipes")
-async def read_user_item(query: str, count: int = 5, location: str = 'CA'):
+async def query_recipes(query: str, count: int = 5, location: str = 'CA'):
     recipe_count, recipes = query_df(query, count)
+    recipes['tags'] = recipes['tags'].apply(lambda x: literal_eval(str(x)))
     print('query: {}, location: {}, recipe count: {}'.format(query, location, recipe_count))
     return recipes.to_json(orient="records")
+
+@app.get("/seasonal")
+async def query_seasonal(location: str, month: str, period: str = 'early'):
+    queried_food = season_df.query('state=="{}" & month=="{}" & period=="{}"'.format(location, month, period))
+    queried_food['foods'] = queried_food['foods'].apply(lambda x: literal_eval(str(x)))
+    return queried_food.to_json(orient="records")
