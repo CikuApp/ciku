259eb863c24f8abd36728c1d4740c0b46365a07f
chriskok <pherkok@gmail.com>
2021-04-16 17:17:18 -0500

    Updated API with documentation

--- backend/api/README.md
+++ backend/api/README.md
@@ -5,7 +5,5 @@
 - Start the server using: `uvicorn recipe_api:app --reload`
 - The server should start on your localhost, probably at this address: http://127.0.0.1:8000
 - To query it from the FrontEnd or on your browser, send a request here: http://127.0.0.1:8000/recipes?query=mash%20potato
-- Note: '%20' here denotes a spacebar so use that as a replacement when necessary
-- Optional arguments:
-  - number of responses: http://127.0.0.1:8000/recipes?query=mash%20potato&count=10
-  - location: http://127.0.0.1:8000/recipes?query=mash%20potato&location=IA
+  - Note: '%20' here denotes a spacebar so use that as a replacement when necessary
+- Check out http://127.0.0.1:8000/docs for all the additional arguments and possible values!
--- backend/api/recipe_api.py
+++ backend/api/recipe_api.py
@@ -1,10 +1,42 @@
-from fastapi import FastAPI
 import pandas as pd
 import numpy as np
 import json
 from ast import literal_eval
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "recipes",
+        "description": "Retrieve a number of recipes based on a query. Variables:\
+        \n- query: str (what to search database for. Note: whitespaces should be replaced with %20)\
+        \n- k: int (number of items requested)",
+    },
+    {
+        "name": "seasonal",
+        "description": "Get all the relevant seasonal and local food for a state and month. Variables:\
+        \n- location: str (state of choice, possible values = 'alaska', 'alabama', 'arkansas', 'arizona', 'california',\
+       'colorado', 'connecticut', 'delaware', 'florida', 'georgia',\
+       'hawaii', 'iowa', 'idaho', 'illinois', 'indiana', 'kansas',\
+       'kentucky', 'louisiana', 'massachusetts', 'maryland', 'maine',\
+       'michigan', 'minnesota', 'missouri', 'mississippi', 'montana',\
+       'north-carolina', 'north-dakota', 'nebraska', 'new-hampshire',\
+       'new-jersey', 'new-mexico', 'nevada', 'new-york', 'ohio',\
+       'oklahoma', 'oregon', 'pennsylvania', 'rhode-island',\
+       'south-carolina', 'south-dakota', 'tennessee', 'texas', 'utah',\
+       'virginia', 'vermont', 'washington', 'wisconsin', 'west-virginia','wyoming')\
+        \n- month: str (month of choice, possible values = 'january', 'february', 'march', 'april', 'may', 'june', 'july',\
+       'august', 'september', 'october', 'november', 'december')\
+        \n- period: str (period within month, possible values = 'early', 'late')",
+    },
+]
+
+app = FastAPI(
+    title="Project INSERT-NAME-HERE",
+    description="An API to support an awesome website for more sustainable recipe selection and grocery shopping!",
+    version="1.0.0",
+    openapi_tags=tags_metadata,
+)
 
 df = pd.read_csv('../data/recipes/RAW_recipes.csv')
 df = df.dropna()
@@ -23,16 +55,15 @@ def query_df(query, k):
 
     return recipe_count, return_data
 
-
-@app.get("/recipes")
-async def query_recipes(query: str, count: int = 5, location: str = 'CA'):
+@app.get("/recipes", tags=["recipes"])
+async def query_recipes(query: str, count: int = 5):
     recipe_count, recipes = query_df(query, count)
     recipes['tags'] = recipes['tags'].apply(lambda x: literal_eval(str(x)))
-    print('query: {}, location: {}, recipe count: {}'.format(query, location, recipe_count))
+    print('query: {}, recipe count: {}'.format(query, recipe_count))
     return recipes.to_json(orient="records")
 
-@app.get("/seasonal")
-async def query_seasonal(location: str, month: str, period: str = 'early'):
+@app.get("/seasonal", tags=["seasonal"])
+async def query_seasonal_foods(location: str, month: str, period: str = 'early'):
     queried_food = season_df.query('state=="{}" & month=="{}" & period=="{}"'.format(location, month, period))
     queried_food['foods'] = queried_food['foods'].apply(lambda x: literal_eval(str(x)))
     return queried_food.to_json(orient="records")
