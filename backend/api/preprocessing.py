89fdee49b03a4bf7bcec61273398426233d3dbd4
chriskok <pherkok@gmail.com>
2021-04-29 06:50:41 -0500

    Updated preprocessing for scraped images

--- backend/api/preprocessing.py
+++ backend/api/preprocessing.py
@@ -6,13 +6,15 @@ from datetime import datetime
 import csv
 
 def clean_recipes():
-    df = pd.read_csv('../data/recipes/RAW_recipes.csv')
+    df = pd.read_csv('../data/img_recipes.csv')
     df = df.dropna()
 
     # TODO: doesn't really work to save as CSV, try different methods in the future
     # df['tags'] = df['tags'].apply(literal_eval)
     # df['ingredients'] = df['ingredients'].apply(literal_eval)
     # df['steps'] = df['steps'].apply(literal_eval)
+
+    print(df.shape)
 
     df.to_csv('../data/clean_recipes.csv', index=False)
 
@@ -23,8 +25,8 @@ def clean_seasonal():
     season_df.to_csv('../data/clean_seasonality.csv', index=False)
 
 def main():
-    # clean_recipes()
-    clean_seasonal()
+    clean_recipes()
+    # clean_seasonal()
 
 if __name__ == '__main__':
     main()
