291fb2aedc044fc5241a38d1c3f423502441893e
Sandy S. Kuo <kuo.sandys@gmail.com>
2021-05-16 18:14:47 +0200

    Remove functionality for saving shopping list

--- frontend/src/components/ShoppingListPage/SaveShoppingListButton.js
+++ frontend/src/components/ShoppingListPage/SaveShoppingListButton.js
@@ -1,20 +1,8 @@
 import React from "react";
 
-import html2canvas from "html2canvas";
-import { saveAs } from "file-saver";
-
 import { Button } from "components/Presentation";
 
 const SaveShoppingListButton = () => {
-  const handleClick = () => {
-    html2canvas(document.querySelector("#shopping-list")).then((canvas) => {
-      canvas.style.display = "none";
-      document.body.appendChild(canvas);
-      canvas.toBlob((blob) => saveAs(blob, "my-shopping-list.png"));
-      document.body.removeChild(canvas);
-    });
-  };
-
   return (
     <Button type="secondary" size="sm" onClick={null}>
       Save List
