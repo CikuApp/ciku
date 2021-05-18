dc78d851d9422bee3b3dd26b7ff6b05da6e3f244
Sandy S. Kuo <kuo.sandys@gmail.com>
2021-05-18 12:52:11 +0200

    Fix Home Page spacing

--- frontend/src/pages/HomePage/Information.js
+++ frontend/src/pages/HomePage/Information.js
@@ -11,23 +11,21 @@ import deliciousMealImage from "assets/images/brooke-lark-nTZOILVZuOg-unsplash-1
 const Information = () => {
   return (
     <PageSectionContainer bgClasses="bg-homepage" displayClasses="flex h-112">
-      <section className="w-6/12 pt-12 overflow-hidden">
+      <section className="w-6/12 pt-12">
         <img
           src={deliciousMealImage}
           alt="A Delicious Meal"
-          className="my-10 lg:my-0 rounded-xl"
+          className="my-10 lg:my-0 rounded-md shadow-lg"
         />
       </section>
-      <section className="w-6/12 pl-24 pt-8 flex items-center">
-        <div className="h-3/5 flex flex-col justify-between">
-          <Heading type="h2">
-            Find out which fruits and vegetables are currently in season.
-          </Heading>
-          <Text type="xl" variant="medium">
-            Select your state below to get started
-          </Text>
-          <LocationSelectorForm />
-        </div>
+      <section className="w-6/12 px-12 pt-8 space-y-8 my-auto">
+        <Heading type="h2">
+          Find out which fruits and vegetables are currently in season.
+        </Heading>
+        <Text type="xl" variant="medium">
+          Select your state below to get started
+        </Text>
+        <LocationSelectorForm />
       </section>
     </PageSectionContainer>
   );
--- frontend/src/pages/HomePage/SustainabilityScoreInfo.js
+++ frontend/src/pages/HomePage/SustainabilityScoreInfo.js
@@ -14,39 +14,38 @@ import exampleRecipe from "data/exampleRecipe";
 const SustainabilityScoreInfo = () => {
   return (
     <PageSectionContainer bgClasses="bg-homepage-2" displayClasses="flex h-112">
-      <section className="w-6/12 pr-36">
-        <div className="h-full flex flex-col justify-between">
-          <Heading type="h3">
-            Get recipe suggestions from Ciku to help you eat more sustainably.
-          </Heading>
-          <Text type="md">
-            Ciku’s recipe suggestions have{" "}
-            <Text variant="em">sustainability scores</Text> to help you identify
-            which recipes include more sustainable ingredients.
-          </Text>
-          <Text type="md">
-            An ingredient that is both seasonal and local receives +2 points. If
-            it’s only seasonal, it receives +1 point.
-          </Text>
-          <Text type="md">
-            Add up the points, and you get the sustainability score! The higher
-            the number, the more sustainable the recipe.
-          </Text>
-        </div>
+      <section className="w-6/12 space-y-8 pr-36 my-auto">
+        <Heading type="h3">
+          Get recipe suggestions from Ciku to help you eat more sustainably.
+        </Heading>
+        <Text type="md">
+          Ciku’s recipe suggestions have{" "}
+          <Text variant="em">sustainability scores</Text> to help you identify
+          which recipes include more sustainable ingredients.
+        </Text>
+        <Text type="md">
+          An ingredient that is both seasonal and local receives +2 points. If
+          it’s only seasonal, it receives +1 point.
+        </Text>
+        <Text type="md">
+          Add up the points, and you get the sustainability score! The higher
+          the number, the more sustainable the recipe.
+        </Text>
       </section>
       <section className="w-6/12 flex items-end">
-        <div className="w-1/4 pb-36 text-center">
+        <div className="w-1/4 pb-32 text-center">
           <Text type="lg" variant="semibold">
             Sustainability Score
           </Text>
         </div>
-        <div className="w-1/4 pt-3 pr-3 pb-20">
+        <div className="w-auto h-24 pr-3 -ml-3 mb-16">
           <img
             src={arrowImage}
             alt="Arrow pointing to sustainability score information."
+            className="h-full w-full object-contain"
           ></img>
         </div>
-        <div className="w-1/2 h-full flex items-center pointer-events-none">
+        <div className="w-1/2 mb-10 pointer-events-none">
           <RecipeCard recipeObject={exampleRecipe} />
         </div>
       </section>
